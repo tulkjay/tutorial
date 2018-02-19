6c5add32e17824a276f739e2f399663fdf6e66a5
Jay Tulk <jay.tulk@selu.edu>
2018-02-19 13:42:30 -0600

    update Chapter 4 exercises

--- exercises.sql
+++ exercises.sql
@@ -58,9 +58,8 @@ WHERE ([InvoiceTotal] - [PaymentTotal] + [CreditTotal] <= 0.00 AND PaymentDate I
 ORDER BY PaymentDate
 
 --Chapter 4
---Chapter 4
 --1
-SELECT * from Vendors, Invoices
+SELECT * from Vendors JOIN Invoices ON Vendors.VendorID = Invoices.VendorID
 
 --2
 SELECT DISTINCT
@@ -68,7 +67,9 @@ v.VendorName,
 i.InvoiceNumber,
 i.InvoiceDate,
 i.InvoiceTotal - i.PaymentTotal + i.CreditTotal AS Balance
-FROM Vendors v JOIN Invoices i ON i.InvoiceTotal - i.PaymentTotal + i.CreditTotal > 0
+FROM Vendors v JOIN Invoices i ON v.VendorID = i.VendorID
+WHERE i.InvoiceTotal - i.PaymentTotal + i.CreditTotal > 0
+ORDER BY v.VendorName
 
 --3
 SELECT
@@ -89,28 +90,32 @@ i.InvoiceDate AS 'Date',
 i.InvoiceNumber AS Number,
 li.InvoiceSequence AS '#',
 li.InvoiceLineItemAmount AS LineItem
-FROM Vendors v, Invoices i, InvoiceLineItems li
+FROM Vendors v
+	JOIN Invoices i ON v.VendorID = i.VendorID
+	JOIN InvoiceLineItems li ON i.InvoiceID = li.InvoiceID
 ORDER BY v.VendorName, i.InvoiceDate, i.InvoiceNumber, li.InvoiceSequence
 
 --6
-SELECT v1.VendorName, v1.VendorID, CONCAT(v1.VendorContactFName, ' ', v1.VendorContactLName) AS 'Name'
-FROM Vendors v1
-JOIN Vendors v2 ON v1.VendorContactFName = v2.VendorContactFName AND v1.VendorName <> v2.VendorName
+SELECT v1.VendorName,
+	v1.VendorID,
+	CONCAT(v1.VendorContactFName, ' ', v1.VendorContactLName) AS 'Name'
+FROM Vendors v1 JOIN Vendors v2
+ON v1.VendorContactFName = v2.VendorContactFName AND v1.VendorID <> v2.VendorID
 ORDER BY Name
 
 --7
 SELECT ga.AccountNo, ga.AccountDescription
 FROM GLAccounts ga
-LEFT OUTER JOIN InvoiceLineItems li ON ga.AccountNo = li.AccountNo
+LEFT JOIN InvoiceLineItems li ON ga.AccountNo = li.AccountNo
 WHERE li.AccountNo IS NULL
+ORDER BY ga.AccountNo
 
 --8
-SELECT VendorName, VendorState 
-FROM Vendors 
-WHERE VendorState = 'CA'
+	SELECT VendorName, VendorState 
+	FROM Vendors 
+	WHERE VendorState = 'CA'
 UNION
-SELECT VendorName, 'Outside CA' as VendorState 
-FROM Vendors 
-WHERE VendorState <> 'CA'
+	SELECT VendorName, 'Outside CA' AS VendorState 
+	FROM Vendors 
+	WHERE VendorState <> 'CA'
 ORDER BY VendorName
-
