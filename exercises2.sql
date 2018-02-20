c6313f1bc0764d6255678780d1a18c393b79918e
Jay Tulk <jay.tulk@selu.edu>
2018-02-20 11:21:41 -0600

    update

--- exercises2.sql
+++ exercises2.sql
@@ -87,3 +87,50 @@ WITH MaxInvoice AS
 )
 SELECT SUM(InvoiceMax) AS SumOfMaximums
 FROM MaxInvoice;
+
+--Chapter 7
+--1
+DROP TABLE VendorCopy
+SELECT * INTO VendorCopy
+FROM Vendors
+
+DROP TABLE InvoiceCopy
+SELECT * INTO InvoiceCopy
+FROM Invoices
+
+--2
+INSERT INTO InvoiceCopy (VendorID, InvoiceTotal, TermsID, InvoiceNumber, PaymentTotal, InvoiceDueDate, InvoiceDate, CreditTotal, PaymentDate)
+VALUES (32, 434.58, 2, 'AX-014-027', 0.00, '07/8/2016', '06/21/2016', 0.00, NULL)
+
+--3
+BEGIN TRAN
+INSERT INTO VendorCopy
+SELECT [VendorName], [VendorAddress1], [VendorAddress2], [VendorCity], [VendorState], [VendorZipCode], [VendorPhone], [VendorContactLName], [VendorContactFName], [DefaultTermsID], [DefaultAccountNo] FROM Vendors
+WHERE VendorState <> 'CA'
+ROLLBACK TRAN
+
+--4
+UPDATE VendorCopy
+SET DefaultAccountNo = 403
+WHERE DefaultAccountNo = 400
+
+--5
+UPDATE InvoiceCopy
+SET PaymentDate = GETDATE(),
+PaymentTotal = InvoiceTotal - PaymentTotal + CreditTotal
+WHERE InvoiceTotal - PaymentTotal + CreditTotal > 0
+
+--6
+UPDATE InvoiceCopy
+SET TermsID = 2
+WHERE InvoiceCopy.VendorID IN (
+	SELECT VendorCopy.VendorID FROM VendorCopy
+	WHERE VendorCopy.DefaultTermsID = 2
+)
+
+--7
+UPDATE InvoiceCopy
+SET TermsID = 2
+FROM InvoiceCopy i JOIN VendorCopy v
+ON i.VendorID = v.VendorID
+WHERE v.DefaultTermsID = 2
